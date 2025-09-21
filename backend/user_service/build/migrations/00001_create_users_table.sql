2ff5c264dc413d62107b22b1534b323b2a5057f6
Alexey Metlushko <leha.metlushko@bk.ru>
2025-09-21 14:56:06 +0300

    backend #17: Created docker-compose.yml for local development, developed users table schema

---
+++ backend/user_service/build/migrations/00001_create_users_table.sql
@@ -0,0 +1,7 @@
+ CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    pubkey_auth TEXT NOT NULL,
+    pubkey_enc TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
